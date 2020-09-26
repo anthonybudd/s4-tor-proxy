08ed2ad6cef576a6a06050d2512eacbca6ad0ca7
Anthony Budd <anthonybudd94@gmail.com>
2020-09-26 13:03:02 -0500

    init

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+FROM ubuntu:16.04
+
+# Copied wholesale from strm/tor-hiddenservice-nginx
+
+# Base packages
+RUN apt-get update && \
+    apt-get -y install \
+    nginx \
+    wget \
+    tor torsocks ntpdate
+
+# Compile shallot
+ADD ./shallot /shallot
+RUN apt-get -y install \
+    build-essential \
+    libssl-dev && \
+    cd /shallot && \
+    ./configure && \
+    make && \
+    mv ./shallot /bin && \
+    cd / && \
+    rm -Rf /shallot && \
+    apt-get -y purge build-essential libssl-dev && \
+    rm -Rf /var/lib/apt/lists/*
+
+# Security and permissions
+RUN useradd --system --uid 666 -M --shell /usr/sbin/nologin hidden
+
+# Configure nginx logs to go to Docker log collection (via stdout/stderr)
+RUN ln --symbolic --force /dev/stdout /var/log/nginx/access.log
+RUN ln --symbolic --force /dev/stderr /var/log/nginx/error.log
+
+# Main script
+ADD ./main.sh /main.sh
+
+# Tor Config
+ADD ./torrc /etc/tor/torrc
+
+# Add nginx default configuration 
+ADD ./nginx.conf /etc/nginx/nginx.conf
+
+# Configuration files and data output folder
+VOLUME /web
+WORKDIR /web
+
+ENTRYPOINT ["/main.sh"]
+CMD ["serve"]
---
+++ main.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ "$1" == "generate" ]
+then
+    if [ -f /web/private_key ]
+    then
+        echo '* You already have an private key, delete it if you want to generate a new key'
+        exit -1
+    fi
+    if [ -z "$2" ]
+    then
+        echo '* You have not provided any mask. Please provide a mask argument to generate your address'
+        exit -1
+    else
+        echo '* Generating the address with mask: '$2
+        shallot -f /tmp/key $2
+        echo '* '$(grep Found /tmp/key)
+        grep 'BEGIN RSA' -A 99 /tmp/key > /web/private_key
+    fi
+
+    address=$(grep Found /tmp/key | cut -d ':' -f 2 )
+
+    echo $address > /web/hostname
+    
+    echo '* Generating site.conf'
+
+    echo 'server {' > /web/site.conf
+    echo '  listen 127.0.0.1:8080;' >> /web/site.conf
+    echo '  server_name '$address';' >> /web/site.conf
+    echo '  ignore_invalid_headers off;' >> /web/site.conf
+    echo '  client_max_body_size 0;' >> /web/site.conf
+    echo '  proxy_buffering off;' >> /web/site.conf
+    echo '' >> /web/site.conf
+    echo '  location / {' >> /web/site.conf
+    echo '    proxy_pass http://minio:9000;' >> /web/site.conf
+    echo '    proxy_set_header X-Real-IP $remote_addr;' >> /web/site.conf
+    echo '    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;' >> /web/site.conf
+    echo '    proxy_set_header X-Forwarded-Proto $scheme;' >> /web/site.conf
+    echo '    proxy_set_header Host $http_host;' >> /web/site.conf
+    echo '    proxy_http_version 1.1;' >> /web/site.conf
+    echo '    proxy_set_header Connection "";' >> /web/site.conf
+    echo '    chunked_transfer_encoding off;' >> /web/site.conf
+    echo '  }' >> /web/site.conf
+    echo '}' >> /web/site.conf
+
+fi
+
+if [ "$1" == "serve" ]
+then
+    if [ ! -f /web/private_key ]
+    then
+        echo '* Please run this container with generate argument to initialize tor-proxy'
+        exit -1
+    fi
+    echo '* Initializing local clock'
+    ntpdate -B -q 0.debian.pool.ntp.org
+    echo '* Starting tor'
+    tor -f /etc/tor/torrc &
+    echo '* Starting nginx'
+    nginx &
+    sleep infinity
+fi
